b0014d80f5ba7bbb89a940dd372ed1255156c23d
Nathaniel <stoddard.nathaniel@yahoo.com>
2025-06-19 10:36:04 -0500

    expirations test

--- app/api/integrations/route.ts
+++ app/api/integrations/route.ts
@@ -3,6 +3,7 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export async function GET(request: NextRequest) {
   try {
