175360ed2aae3506396ff74d55f03221ec358c95
Nathaniel <stoddard.nathaniel@yahoo.com>
2025-06-23 22:36:00 -0500

    landing page updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import PublicLayout from "@/components/layout/PublicLayout"
 import LandingPage from "@/components/landing/LandingPage"
 
 export default function HomePage() {
-  return (
-    <PublicLayout>
-      <LandingPage />
-    </PublicLayout>
-  )
+  return <LandingPage />
 }
