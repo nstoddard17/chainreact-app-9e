f789aab7c48c34fc61addae21a7826a33706aea4
Marcus Leonard <marcusleonard120@gmail.com>
2026-01-27 21:03:04 -0600

    update

--- components/apps/AppsWorkspaceGroupView.tsx
+++ components/apps/AppsWorkspaceGroupView.tsx
@@ -91,9 +91,9 @@ export function AppsWorkspaceGroupView({ integrations, renderAppCard }: AppsWork
         </button>
 
         {!isCollapsed && (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pl-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pl-4 auto-rows-fr items-stretch">
             {apps.map(integration => (
-              <div key={integration.id}>
+              <div key={integration.id} className="h-full">
                 {renderAppCard(integration)}
               </div>
             ))}
