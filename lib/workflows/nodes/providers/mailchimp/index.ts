6122222bd7919f8f90ab02b2263e9e4904d933a9
Marcus Leonard <marcusleonard120@gmail.com>
2026-02-22 12:24:40 -0600

    fixed polling for mailchimp

--- lib/workflows/nodes/providers/mailchimp/index.ts
+++ lib/workflows/nodes/providers/mailchimp/index.ts
@@ -548,6 +548,7 @@ export const mailchimpNodes: NodeComponent[] = [
     providerId: "mailchimp",
     category: "Email",
     isTrigger: true,
+    triggerType: "polling",
     producesOutput: true,
     configSchema: [
       {
